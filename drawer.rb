d7921a47ec393e9614c0616bc5983b6f41d9481a
sonota88 <yosiot8753@gmail.com>
2020-02-15 09:33:25 +0900

    具体的な描画処理を Drawer クラスにカプセル化

---
+++ drawer.rb
@@ -0,0 +1,41 @@
+require "plumo"
+
+class Drawer
+  def initialize(ppc)
+    @ppc = ppc
+
+    @plumo = Plumo.new(640, 480)
+    @plumo.start
+  end
+
+  def to_web_rgba(color)
+    rgba = color[0..2]
+
+    rgba <<
+      if color.size == 4
+        color[3].to_f / 255
+      else
+        1.0
+      end
+
+    "rgba(%s)" % rgba.join(", ")
+  end
+
+  def draw_line(x1, y1, x2, y2, color)
+    @plumo.color to_web_rgba(color)
+    @plumo.line(
+      x1 * @ppc, y1 * @ppc,
+      x2 * @ppc, y2 * @ppc
+    )
+  end
+
+  def draw_box(x1, y1, x2, y2, color)
+    w = x2 - x1
+    h = y2 - y1
+    @plumo.color to_web_rgba(color)
+    @plumo.stroke_rect(
+      x1 * @ppc, y1 * @ppc,
+      w * @ppc, h * @ppc
+    )
+  end
+end
--- viewer.rb
+++ viewer.rb
@@ -1,30 +1,36 @@
-require "plumo"
+# coding: utf-8
+require "./drawer"
 require "./libo_draw"
 
 # pixels per cell
 PPC = 20
 
+# colors
+C_WHITE   = [255, 255, 255, 255]
+
 path = ARGV[0]
 
 doc = LiboDraw::Document.new(path)
 
-plumo = Plumo.new(640, 480)
-plumo.start
-
-plumo.color "#fff"
+drawer = Drawer.new(PPC)
 
 doc.pages[0].rectangles.each { |rect|
-  plumo.stroke_rect(
-    rect.x * PPC, rect.y * PPC,
-    rect.w * PPC, rect.h * PPC
+  x2 = rect.x + rect.w
+  y2 = rect.y + rect.h
+  drawer.draw_box(
+    rect.x, rect.y,
+    x2,     y2,
+    C_WHITE
   )
 }
 
 doc.pages[0].lines.each { |line|
-  plumo.line(
-    line.x1 * PPC, line.y1 * PPC,
-    line.x2 * PPC, line.y2 * PPC
+  drawer.draw_line(
+    line.x1, line.y1,
+    line.x2, line.y2,
+    C_WHITE
   )
 }
 
+# 描画する前に終了しないように待つ
 sleep 60
