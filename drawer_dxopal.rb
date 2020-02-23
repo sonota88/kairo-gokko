30ef59096be21caec1e7d5c777f542d6fe1c73d4
sonota88 <yosiot8753@gmail.com>
2020-02-23 15:58:05 +0900

    Add drawer_dxopal.rb

---
+++ drawer_dxopal.rb
@@ -0,0 +1,13 @@
+class Drawer
+  def initialize(ppc)
+    @ppc = ppc
+  end
+
+  def draw_line(x1, y1, x2, y2, color)
+    Window.draw_line(
+      x1 * @ppc, y1 * @ppc,
+      x2 * @ppc, y2 * @ppc,
+      color
+    )
+  end
+end
