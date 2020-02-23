e268b802cb87fb75265baaf87fc131cf3fb95f5b
sonota88 <yosiot8753@gmail.com>
2020-02-23 13:27:12 +0900

    Restore circuit from data.json

--- viewer.rb
+++ viewer.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
+
 require "./circuit"
 require "./unit"
-require "./libo_draw"
 require "./view"
 
 # pixels per cell
@@ -12,21 +12,9 @@ C_WHITE   = [255, 255, 255, 255]
 
 # --------------------------------
 
-path = ARGV[0]
-
-page_no =
-  if ARGV[1]
-    ARGV[1].to_i
-  else
-    1
-  end
-
-doc = LiboDraw::Document.new(path)
-page = doc.pages[page_no - 1]
-
-circuit = Circuit.create(
-  page.lines,
-  page.rectangles
+json = File.read("data.json")
+circuit = Circuit.from_plain(
+  JSON.parse(json)
 )
 
 view = View.new(PPC)
