9954a07d582039a406aa6d64ef467d139fc3c6fa
sonota88 <yosiot8753@gmail.com>
2020-03-09 05:10:03 +0900

    Add Circuit#to_plain and Circuit.from_plain

--- circuit.rb
+++ circuit.rb
@@ -4,4 +4,20 @@ class Circuit
   def initialize(child_circuits)
     @child_circuits = child_circuits
   end
+
+  def to_plain
+    {
+      child_circuits: @child_circuits.map { |child_circuit| child_circuit.to_plain }
+    }
+  end
+
+  def self.from_plain(data)
+    child_circuits =
+      data["child_circuits"]
+        .map { |child_circuit_data|
+          ChildCircuit.from_plain(child_circuit_data)
+        }
+
+    Circuit.new(child_circuits)
+  end
 end
