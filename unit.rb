cd642269643282666397642e6a128c18e1fe87bf
sonota88 <yosiot8753@gmail.com>
2020-03-11 03:15:35 +0900

    Add Unit::Lamp

--- unit.rb
+++ unit.rb
@@ -224,6 +224,26 @@ module Unit
     end
   end
 
+  class Lamp < SingleCell
+    def initialize(pos)
+      super
+
+      # ON: true / OFF: false
+      @state = false
+    end
+
+    def to_plain
+      {
+        pos: @pos.to_plain
+      }
+    end
+
+    def self.from_plain(plain)
+      Lamp.new(
+        Point.from_plain(plain["pos"])
+      )
+    end
+  end
 end
 
 def Point(x, y)
