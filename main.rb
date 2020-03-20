82629998dc734cee1833e4cdc1692d8818a00af1
sonota88 <yosiot8753@gmail.com>
2020-03-20 15:50:00 +0900

    Change update interval sec: 0.2 => 0.1

--- main.rb
+++ main.rb
@@ -114,7 +114,7 @@ def on_push_switch(pushed_switch)
 end
 
 def update_tuden_relay_switch_lamp(circuit)
-  return if Time.now < circuit.last_update + 0.2
+  return if Time.now < circuit.last_update + 0.1
   circuit.last_update = Time.now
 
   circuit.update_tuden_state()
