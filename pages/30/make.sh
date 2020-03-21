9dc6de1c50b8f57da7498540024e7176d1471f02
sonota88 <yosiot8753@gmail.com>
2020-03-22 08:54:07 +0900

    Add step 30

---
+++ pages/30/make.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+print_file_list(){
+  cat <<EOB
+child_circuit.rb
+circuit.rb
+data.rb
+drawer_dxopal.rb
+main.rb
+tuden.rb
+unit.rb
+view.rb
+EOB
+}
+
+copy(){
+  while read -r file; do
+    cp "../../${file}" .
+  done
+}
+
+print_file_list | copy
+
+mv main.rb main.rb.orig
+cat main.rb.orig \
+  | sed -e 's/click\.wav/\.\.\/click\.wav/' \
+  | sed -e 's/relay\.wav/\.\.\/relay\.wav/' \
+  | sed -e 's/PPC = 30/PPC = 20/' \
+  > main.rb
+rm main.rb.orig
