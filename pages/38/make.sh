9d51c75e40745f91663d1dbcdb0cf6b9e10feb1b
sonota88 <yosiot8753@gmail.com>
2020-08-01 07:33:05 +0900

    Add step 38

---
+++ pages/38/make.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+print_file_list(){
+  cat <<EOB
+child_circuit.rb
+circuit.rb
+data.rb
+drawer_dxopal.rb
+main.rb
+unit.rb
+view.rb
+EOB
+}
+
+copy(){
+  while read -r file; do
+    cp "../../${file}" .
+  done
+}
+
+print_file_list | copy
+
+mv main.rb main.rb.orig
+cat main.rb.orig \
+  | sed -e 's/click\.wav/\.\.\/click\.wav/' \
+  | sed -e 's/relay_2\.wav/\.\.\/relay_2\.wav/' \
+  > main.rb
+rm main.rb.orig
