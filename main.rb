84017fc2d3a33a0d506346ad359c2fa80c4f2285
sonota88 <yosiot8753@gmail.com>
2020-08-23 13:27:01 +0900

    Log the time spent on all require_remote (main.rb)

--- main.rb
+++ main.rb
@@ -13,6 +13,8 @@ def update_progress(msg)
   end
 end
 
+t_before_require = Time.now
+
 if browser?
   require "dxopal"
   update_progress "dxopal"
@@ -42,6 +44,8 @@ def parse_json(json)
     JSON.parse(json)
   end
 end
+
+puts format("require done: %.02f sec", Time.now - t_before_require)
 
 def get_els(selector, el = Native(`document`))
   el.querySelectorAll(selector)
